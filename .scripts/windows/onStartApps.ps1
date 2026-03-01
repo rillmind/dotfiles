57d453203e716c3073ac43617d2c67cc64c456da
Raul Holanda Lopes <raulzc00@gmail.com>
2026-02-28 22:52:28 -0300

    Atualizando...

--- .scripts/windows/onStartApps.ps1
+++ .scripts/windows/onStartApps.ps1
@@ -1,15 +1,16 @@
 # Inicia os aplicativos que não precisam de privilégios
-Start-Process -FilePath "C:\Users\raios\AppData\Local\FlowLauncher\Flow.Launcher.exe"
 Start-Process -FilePath "C:\Program Files\Zen Browser\zen.exe"
-Start-Process -FilePath "C:\Users\sackr\AppData\Roaming\Spotify\Spotify.exe"
+Start-Process -FilePath "C:\Users\raios\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Spotify.lnk"
+Start-Process -FilePath "C:\Program Files\glzr.io\GlazeWM\glazewm.exe"
+Start-Process -FilePath "C:\Users\raios\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\Discord.lnk"
 
-# Inicia o Riot Client com seus argumentos específicos
-$riotArgs = @(
-    "--launch-product=league_of_legends",
-    "--launch-patchline=live"
-)
-Start-Process -FilePath "C:\Riot Games\Riot Client\RiotClientServices.exe" -ArgumentList $riotArgs
+# # Inicia o Riot Client com seus argumentos específicos
+# $riotArgs = @(
+#     "--launch-product=league_of_legends",
+#     "--launch-patchline=live"
+# )
+
+# Start-Process -FilePath "C:\Riot Games\Riot Client\RiotClientServices.exe" -ArgumentList $riotArgs
 
 # Inicia o GlazeWM solicitando elevação de administrador (UAC)
 # O -Verb RunAs é o equivalente a "Executar como administrador"
-Start-Process -FilePath "C:\Program Files\glzr.io\GlazeWM\glazewm.exe"
