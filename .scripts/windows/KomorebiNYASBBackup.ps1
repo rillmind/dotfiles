0604a8072ce9ea7cef62c87882265ad17e7992ff
Raul Holanda Lopes <raulzc00@gmail.com>
2026-05-01 18:15:32 -0300

    Atualizando...

---
+++ .scripts/windows/KomorebiNYASBBackup.ps1
@@ -0,0 +1,6 @@
+cat .\komorebi.json > .\dotfiles\.config\komorebi\komorebi.json
+cat .\komorebi.bar.json > .\dotfiles\.config\komorebi\komorebi.bar.json
+cat .\applications.json > .\dotfiles\.config\komorebi\applications.json
+cat .\.config\whkdrc > .\dotfiles\.config\komorebi\.config\whkdrc
+cat .\.config\yasb\config.yaml > .\dotfiles\.config\yasb\config.yaml
+cat .\.config\yasb\styles.css > .\dotfiles\.config\yasb\styles.css
--- .scripts/windows/onStartApps.ps1
+++ .scripts/windows/onStartApps.ps1
@@ -1,8 +1,8 @@
 # Inicia os aplicativos que não precisam de privilégios
-Start-Process -FilePath "C:\Program Files\Zen Browser\zen.exe"
-Start-Process -FilePath "C:\Users\raios\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Spotify.lnk"
-Start-Process -FilePath "C:\Program Files\glzr.io\GlazeWM\glazewm.exe"
-Start-Process -FilePath "C:\Users\raios\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\Discord.lnk"
+# Start-Process -FilePath "C:\Program Files\Zen Browser\zen.exe"
+# Start-Process -FilePath "C:\Users\raios\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Spotify.lnk"
+# Start-Process -FilePath "C:\Program Files\glzr.io\GlazeWM\glazewm.exe"
+# Start-Process -FilePath "C:\Users\raios\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\Discord.lnk"
 
 # # Inicia o Riot Client com seus argumentos específicos
 # $riotArgs = @(
@@ -14,3 +14,7 @@ Start-Process -FilePath "C:\Users\raios\AppData\Roaming\Microsoft\Windows\Start 
 
 # Inicia o GlazeWM solicitando elevação de administrador (UAC)
 # O -Verb RunAs é o equivalente a "Executar como administrador"
+
+komorebic start --whkd
+Start-Process -FilePath "C:\Users\raios\AppData\Local\FlowLauncher\Flow.Launcher.exe"
+yasb
