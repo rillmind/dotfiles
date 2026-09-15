45130061f988c3d5a147408a13c5dc183badcf0d
Raul Holanda Lopes <raulzc00@gmail.com>
2026-09-15 12:19:10 -0300

    Atualizando...

--- .scripts/windows/onStartApps.ps1
+++ .scripts/windows/onStartApps.ps1
@@ -1,22 +1,12 @@
-# Inicia os aplicativos que não precisam de privilégios
-# Start-Process -FilePath "C:\Program Files\Zen Browser\zen.exe"
-# Start-Process -FilePath "C:\Users\raios\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Spotify.lnk"
-# Start-Process -FilePath "C:\Program Files\glzr.io\GlazeWM\glazewm.exe"
-# Start-Process -FilePath "C:\Users\raios\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\Discord.lnk"
+# O '-Verb RunAs' é o equivalente a "Executar como administrador"
 
-# # Inicia o Riot Client com seus argumentos específicos
-# $riotArgs = @(
-#     "--launch-product=league_of_legends",
-#     "--launch-patchline=live"
-# )
-
-# Start-Process -FilePath "C:\Riot Games\Riot Client\RiotClientServices.exe" -ArgumentList $riotArgs
-
-# Inicia o GlazeWM solicitando elevação de administrador (UAC)
-# O -Verb RunAs é o equivalente a "Executar como administrador"
+# Em uso:
 
 Start-Process syncthing -ArgumentList "--no-browser" -WindowStyle Hidden
-komorebic start --whkd
-Start-Process -FilePath "C:\Users\raios\AppData\Local\FlowLauncher\Flow.Launcher.exe"
-Start-Process -FilePath "C:\Program Files\Alienware\Alienware Command Center\AWCC\AWCC.exe"
+Start-Process -FilePath "C:\Program Files (x86)\Thermal Control Center\tcc-g15.exe" -Verb RunAs
 yasb
+
+# Em desuso:
+
+#Start-Process -FilePath "C:\Users\raios\AppData\Local\FlowLauncher\Flow.Launcher.exe"
+#komorebic start --whkd
